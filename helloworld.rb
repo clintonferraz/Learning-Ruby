674c7ff781f1ec9c2449ca4dd6b463356d75d90e
Clinton <clinton_ferraz@hotmail.com>
2020-09-17 21:58:13 -0300

    First Commit

---
+++ helloworld.rb
@@ -0,0 +1,9 @@
+
+print "\nHello World!" + "\n"*2
+print "Digite"
+print " 2 números (inteiros): "
+num = gets.chomp.to_i 
+num2 = gets.chomp.to_i 
+puts "A soma entre eles: #{num + num2} \nA subtração entre eles: #{num - num2}"
+puts "A multiplicação entre eles: #{num * num2} \nA divisão (inteira) entre eles: #{num / num2}"
+puts "O resto da divisão entre eles: #{num % num2}"
