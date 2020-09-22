c06cf7a00f59d4950a5ff21184a7f6ff8b3f4832
Clinton <clinton_ferraz@hotmail.com>
2020-09-22 19:18:27 -0300

    Construtor

--- classes.rb
+++ classes.rb
@@ -7,7 +7,7 @@ end
 
 class Son < Father                                     #cria uma classe que herda os atributos e métodos da classe Father
     def print_age                                      #alem dos metodos herdados, a classe Son agora também possui um metodo extra print_age
-        puts "A idade é #{@age} anos"                       #para sobrescrever um método herdado, basta defini-lo novamente (usando o mesmo nome)
+        puts "A idade é #{@age} anos"                  #para sobrescrever um método herdado, basta defini-lo novamente (usando o mesmo nome)
     end
 end
 
@@ -23,3 +23,32 @@ obj_pai.print_name
 obj_filho.print_name
 obj_filho.print_age
 
+class Person
+    attr_accessor :name, :age
+    def initialize(name, age)                          #Construtor(deve se chamar "initialize"): inicializa o objeto com os atributos predefinidos
+        @name = name
+        @age = age
+    end
+    def check_attributes
+        puts "Nome: #{@name}"
+        puts "Idade: #{@age}"
+    end
+end
+
+pers1 = Person.new('Caio', 23)                         #passando atributos pro construtor
+pers1.check_attributes
+pers1.name = "Cezar"
+pers1.check_attributes
+
+class PersonConstAge < Person
+    def initialize(name)                               #Sobrescrevendo o construtor herdado da classe Person, para que a idade seja constante
+        @name = name
+        @age = 19                                      
+    end
+end
+
+pers2 = PersonConstAge.new('Paulo')
+pers2.check_attributes
+
+pers3 = PersonConstAge.new('Roberto')
+pers3.check_attributes
